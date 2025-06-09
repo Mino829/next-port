34a2fae8336dae98daa017342aa997056bab48be
bp24042 <bp24042@shibaura-it.ac.jp>
2025-06-09 10:11:04 +0900

    12

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,15 @@
+import { style } from "framer-motion/client";
+
 export default function AboutPage() {
   return (
     <div className="p-8 text-white">
       <h1 className="text-4xl font-bold mb-4">About</h1>
       <p>これは自己紹介ページです。</p>
-      <>
-      
-      </>      
+      <nav>
+        <h1>
+          こんにちは
+        </h1>
+      </nav>      
     </div>
   )
 }
